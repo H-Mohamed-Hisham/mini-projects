81b985d357faefb374e4a476f72bda3607f65bc1
H-Mohamed-Hisham <hmhwebdev@gmail.com>
2024-05-23 00:31:12 +0530

    Initial Commit

---
+++ palindrome-checker/script.js
@@ -0,0 +1,23 @@
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+const checkPalindrome = () => {
+  let input = textInput.value;
+  if (!input) {
+    alert("Please input a value");
+    return;
+  }
+
+  const cleanedInput = input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  const reversedInput = cleanedInput.split("").reverse().join("");
+
+  if (cleanedInput === reversedInput) {
+    result.textContent = `${input} is a palindrome`;
+  } else {
+    result.textContent = `${input} is not a palindrome`;
+  }
+};
+
+checkBtn.addEventListener("click", checkPalindrome);
